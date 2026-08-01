20959a1d68d7f7d197c98a2d500c1c692b634eec
Maxim Kozlov <maximko.ca@gmail.com>
2026-07-31 23:16:54 -0400

    better player id system

--- engladius_server/main.lua
+++ engladius_server/main.lua
@@ -1,8 +1,26 @@
 io.stdout:setvbuf("no")
 
+function get_mex(arr)
+    local present = {}
+    
+    for _, val in ipairs(arr) do
+        if val >= 0 then
+            present[val] = true
+        end
+    end
+    
+    local mex = 0
+    while present[mex] do
+        mex = mex + 1
+    end
+    
+    return mex
+end
+
 local enet = require("enet")
 local server
 local players = {}
+local player_addr_to_id = {}
 
 ---@diagnostic disable-next-line: duplicate-set-field
 function love.load()
@@ -23,23 +41,37 @@ function love.update(delta)
     local event = server:service(0)
     while event do
         if event.type == "connect" then
-            local id_str = tostring(event.peer)
+
+            local ids = {}
+            for id, _ in pairs(players) do
+                table.insert(ids, tonumber(id))
+            end
+            local mex = get_mex(ids)
+            local id_str = tostring(mex)
             print("Client connected with id " .. id_str)
             players[id_str] = {
                 x = 0,
                 y = 0,
                 peer = event.peer,
             }
+            player_addr_to_id[tostring(event.peer)] = id_str
         end
 
         if event.type == "disconnect" then
-            local id_str = tostring(event.peer)
+            local addr = tostring(event.peer)
+            local id_str = player_addr_to_id[addr]
+            
             print("Client disconnected with id " .. id_str)
             players[id_str] = nil
+            player_addr_to_id[addr] = nil
+
+            
+        
         end
 
         if event.type == "receive" then
-            local id_str = tostring(event.peer)
+            local addr = tostring(event.peer)
+            local id_str = player_addr_to_id[addr]
 
             if event.data[1] == "p" then
                 local player = players[id_str]
